459e6ffde7b72ecffe483dd6bd7d86fd8bc272f9
Ruslan <32275726+takoyzaya@users.noreply.github.com>
2018-06-04 02:29:40 +0300

    Initial commit

---
+++ ChatZaychatAPI/ChatZaychatAPI/Data/MainContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatZaychatAPI.Models
+{
+    public class MainContext : DbContext
+    {
+        private static bool _created = false;
+        public MainContext(DbContextOptions<MainContext> options)
+            : base(options)
+        {
+            if (!_created)
+            {
+                Database.Migrate();
+                _created = true;
+            }
+        }
+
+
+        public DbSet<ChatZaychatAPI.Models.Message> Message { get; set; }
+        public DbSet<ChatZaychatAPI.Models.UserContacts> UserContacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserContacts>()
+                .Property<string>("ExtendedDataStr")
+                .HasField("_extendedData");
+        }
+    }
+}
---
+++ ChatZaychatUI/ChatZaychatUI/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ChatZaychatUI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using ChatZaychatUI.Data;
+
+namespace ChatZaychatUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            ViewData["username"] = User.FindFirst(ClaimTypes.Name).Value;
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
